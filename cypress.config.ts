b62506ecda8491ab41eb615a303b71baceae6907
Valeriia <valeria.sinkevich20@gmail.com>
2023-10-15 17:31:05 -0400

    reports Hw

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,17 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implement node event listeners here
     },
     specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4200',
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
 });
+
